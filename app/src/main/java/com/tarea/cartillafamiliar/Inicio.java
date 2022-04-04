b522491bbc1b1633601d974808d1f2b368b0614a
AlonsoPrez01 <alonso.perflores@gmail.com>
2022-04-03 23:51:24 -0500

    Second commit

--- app/src/main/java/com/tarea/cartillafamiliar/Inicio.java
+++ app/src/main/java/com/tarea/cartillafamiliar/Inicio.java
@@ -1,10 +1,14 @@
 package com.tarea.cartillafamiliar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Inicio extends AppCompatActivity {
 
@@ -22,5 +26,19 @@ public class Inicio extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.munu_toolbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_vacuna:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void anniadirVacuna(MenuItem item){
+        Intent anniadirvacuna = new Intent(this, ListaVacunas.class);
+        startActivity(anniadirvacuna);
     }
 }
